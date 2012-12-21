8952979ae24a9687c7053ce2d84f4698dfc19af0
Pramod Rao <pramod@bizosys.com>
2012-12-21 11:11:44 +0530

    Federated Search

--- src/java/com/bizosys/hsearch/idsearch/table/ITermTable.java
+++ src/java/com/bizosys/hsearch/idsearch/table/ITermTable.java
@@ -1,6 +1,7 @@
 package com.bizosys.hsearch.idsearch.table;
 
 import java.io.IOException;
+import java.util.Map;
 
 import com.bizosys.hsearch.treetable.Cell2;
 import com.bizosys.hsearch.treetable.Cell4;
@@ -9,13 +10,19 @@ public interface ITermTable
 {
 	void addSearchData(TermTableRow row);
 	
-	byte[] toBytes(int partitionSeq) throws IOException;
+	byte[] toBytes() throws IOException;
 	
+//	Cell2<Integer, Float> findIdsFromSerializedTableQuery(
+//			byte[] input, TermQuery filter) throws IOException;
+	
+//	Cell4<Integer, Integer, Integer, Float> findValuesFromSerializedTableQuery( 
+//			byte[] input, TermQuery filter) throws IOException;
+
 	Cell2<Integer, Float> findIdsFromSerializedTableQuery(
-			byte[] input, TermQuery filter) throws IOException;
-	
+			byte[] input, Map<String, TermQuery> filterMap) throws IOException;
+
 	Cell4<Integer, Integer, Integer, Float> findValuesFromSerializedTableQuery( 
-		byte[] input, TermQuery filter) throws IOException;
+		byte[] input, Map<String, TermQuery> filterMap) throws IOException;
 	
 	void clear();
 }
--- src/java/com/bizosys/hsearch/idsearch/table/TermTable.java
+++ src/java/com/bizosys/hsearch/idsearch/table/TermTable.java
@@ -2,10 +2,14 @@ package com.bizosys.hsearch.idsearch.table;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.bizosys.hsearch.byteutils.SortedBytesFloat;
 import com.bizosys.hsearch.byteutils.SortedBytesInteger;
@@ -34,38 +38,39 @@ public class TermTable implements ITermTable
 	
 	private void getSearchTable()
 	{
-		File searchFile = new File("f:\\work\\hsearch-pointcross\\reference\\searchdata");
+		File searchFile = new File("f:\\work\\hsearch-idsearch\\reference\\searchdata.tsv");
 		BufferedReader reader = null;
 		InputStream stream = null;
 		
-		/**
 		try 
 		{
 			stream = new FileInputStream(searchFile); 
-			reader = new BufferedReader ( new InputStreamReader (stream) );
+			reader = new BufferedReader ( new InputStreamReader(stream) );
 			
 			String line;
-			SearchData searchData;
+			TermTableRow searchData;
 			while((line=reader.readLine())!=null)
 			{
-				String[] lineVars = line.split("|");
-				searchData = new SearchData();
-				searchData.setParams(Integer.parseInt(lineVars[0]), lineVars[1], Integer.parseInt(lineVars[2]), 
-						Integer.parseInt(lineVars[3]), Integer.parseInt(lineVars[4]), Float.parseFloat(lineVars[5]));
+				String[] lineVars = line.split("\t");
+				searchData = new TermTableRow();
+				searchData.setParams(lineVars[0], Integer.parseInt(lineVars[1]), Integer.parseInt(lineVars[2]), 
+						Integer.parseInt(lineVars[3]), Float.parseFloat(lineVars[4]));
 				addSearchData(searchData);
 			}
 		}
-		catch (Exception e) {
+		catch (Exception e) 
+		{
+			e.printStackTrace();
 		}
-		*/
-		TermTableRow searchData = new TermTableRow();
-		
-		System.out.println("Making Cube Start");
-		for ( int i=0; i<10000000; i++) {
-			searchData.setParams( "Jyoti", 1, 1, i, 0.0f);
-			addSearchData(searchData);
-		}
-		System.out.println("Making Cube Done");
+
+//		TermTableRow searchData = new TermTableRow();
+//		
+//		System.out.println("Making Cube Start");
+//		for ( int i=0; i<10000000; i++) {
+//			searchData.setParams( "Jyoti", 1, 1, i, 0.0f);
+//			addSearchData(searchData);
+//		}
+//		System.out.println("Making Cube Done");
 
 	}
 	
@@ -76,7 +81,7 @@ public class TermTable implements ITermTable
 	}
 
 	@Override
-	public byte[] toBytes(int partitionSeq) throws IOException 
+	public byte[] toBytes() throws IOException 
 	{
 		return fieldHash_field_recordType_fieldType_recordId_fieldWeight.toBytes(new CellComparator.FloatComparator<Integer>());
 	}
@@ -84,13 +89,22 @@ public class TermTable implements ITermTable
 	
 	@Override
 	public Cell2<Integer, Float> findIdsFromSerializedTableQuery(
-			byte[] input, TermQuery filter) throws IOException {
-		return null;
+			byte[] input, Map<String, TermQuery> filterMap) throws IOException 
+	{
+		Cell2<Integer, Float> cell2 = new Cell2<Integer, Float>(SortedBytesInteger.getInstance(), SortedBytesFloat.getInstance());
+		Iterator<Entry<String, TermQuery>> it = filterMap.entrySet().iterator();
+		while(it.hasNext())
+		{
+			Entry<String, TermQuery> entry = it.next();
+			TermQuery queryFilter = entry.getValue();
+			findIdsFromSerializedTableQuery(input, queryFilter, cell2, null);
+		}
+		return cell2;
 	}
 	
 	@Override
 	public Cell4<Integer, Integer, Integer, Float> findValuesFromSerializedTableQuery( 
-		byte[] input, TermQuery filter) throws IOException {
+		byte[] input, Map<String, TermQuery> filterMap) throws IOException {
 		return null;
 		
 	}
@@ -102,47 +116,63 @@ public class TermTable implements ITermTable
 		
 		this.fieldHash_field_recordType_fieldType_recordId_fieldWeight.parseElements(input);
 		
-		Collection<Cell5<String, Integer, Integer, Integer, Float>> cell5L = ( filter.hasField()) ? 
-				this.fieldHash_field_recordType_fieldType_recordId_fieldWeight.values(filter.getFieldCode()) : 
-					this.fieldHash_field_recordType_fieldType_recordId_fieldWeight.values();
-		
-		if(cell5L == null) return;
-		
-		Collection<Cell4<Integer, Integer, Integer, Float>> cell4L = new ArrayList<Cell4<Integer,Integer,Integer,Float>>();
-		Collection<Cell3<Integer, Integer, Float>> cell3L = new ArrayList<Cell3<Integer,Integer,Float>>();			
-		Collection<Cell2<Integer, Float>> cell2L = new ArrayList<Cell2<Integer,Float>>();
+		Map<Integer, Cell5<String, Integer, Integer, Integer, Float>> cell5Map = new HashMap<Integer, Cell5<String, Integer, Integer, Integer, Float>>();
+		Map<Integer, Cell3<Integer, Integer, Float>> cell3Map = new HashMap<Integer, Cell3<Integer, Integer, Float>>();
+		Map<Integer, Cell2<Integer, Float>> cell2Map = new HashMap<Integer, Cell2<Integer, Float>>();
 
-		for (Cell5<String, Integer, Integer, Integer, Float> cell5 : cell5L) 
+		if(filter.hasField()) 
+			this.fieldHash_field_recordType_fieldType_recordId_fieldWeight.getMap(filter.getFieldCode(), null, null, cell5Map);
+		else
+			cell5Map = this.fieldHash_field_recordType_fieldType_recordId_fieldWeight.getMap();
+		 
+		Iterator<Entry<Integer, Cell5<String, Integer, Integer, Integer, Float>>> cell5MapItr = cell5Map.entrySet().iterator();  
+		while (cell5MapItr.hasNext()) 
 		{
-			cell4L.clear();
-			cell5.values(filter.getField(), cell4L);
-			
-			for (Cell4<Integer, Integer, Integer, Float> cell4 : cell4L) 
+			Entry<Integer, Cell5<String, Integer, Integer, Integer, Float>> cell5E = cell5MapItr.next();
+			Cell5<String, Integer, Integer, Integer, Float> cell5 = cell5E.getValue();
+
+			Iterator<Entry<String, Cell4<Integer, Integer, Integer, Float>>> itemItr = cell5.getMap().entrySet().iterator();  
+			while ( itemItr.hasNext()) 
 			{
-				cell3L.clear();
-				if(filter.hasRecordType())
-					cell4.values(filter.getRecordType(), cell3L);
+				Entry<String, Cell4<Integer, Integer, Integer, Float>> aTerm = itemItr.next();
+				Cell4<Integer, Integer, Integer, Float> cell4 = aTerm.getValue();
+
+				if(filter.hasRecordType()) 
+					cell4.getMap(filter.getRecordType(), null, null, cell3Map);
 				else
-					cell4.values(cell3L);
+					cell3Map = cell4.getMap();
 				
-				for (Cell3<Integer, Integer, Float> cell3 : cell3L) 
+				Iterator<Entry<Integer, Cell3<Integer, Integer, Float>>> docItr = cell3Map.entrySet().iterator();
+				while ( docItr.hasNext()) 
 				{
-					cell2L.clear();
-					if(filter.hasFieldType())
-						cell3.values(filter.getFieldType(), cell2L);
-					else
-						cell3.values(cell2L);
-					
-					for (Cell2<Integer, Float> cell2 : cell2L) 
-					{
-						for (CellKeyValue<Integer, Float> kv : cell2.getMap()) {
-							if ( idL != null ) idL.add(kv.getKey(), kv.getValue());
-							//if ( rowL != null ) rowL.put(cell4., k2, k3, v)add(kv.getKey(), kv.getValue());
+					Entry<Integer, Cell3<Integer, Integer, Float>> aDoc = docItr.next();
+					Integer _doc = aDoc.getKey();
+					Cell3<Integer, Integer, Float> cell3 = aDoc.getValue();
 							
+					if(filter.hasFieldType()) 
+						cell3.getMap(filter.getFieldType(), null, null, cell2Map);
+					else
+						cell2Map = cell3.getMap();
+					
+					Iterator<Entry<Integer, Cell2<Integer, Float>>> termtypeItr = cell2Map.entrySet().iterator();
+					while ( termtypeItr.hasNext()) 
+					{
+						Entry<Integer, Cell2<Integer, Float>> word = termtypeItr.next();
+						Integer _wordtype = word.getKey();
+						Cell2<Integer, Float> cell2 = word.getValue();
+									
+						for (CellKeyValue<Integer, Float> _word : cell2.getMap()) 
+						{
+							if(null != rowL)
+							{
+								rowL.put(_doc, _wordtype, _word.getKey(), _word.getValue());
+								System.out.println(_doc +", " +_wordtype +", " +_word.getKey() +", " +_word.getValue());
+							}
+							if(null != idL)
+								idL.add(_word.getKey(), _word.getValue());
 						}
 					}
 				}
-				
 			}
 		}
 	}
@@ -161,13 +191,26 @@ public class TermTable implements ITermTable
 
 	public static void main(String[] args)
 	{
+		TermTable tt = new TermTable();
 		try 
 		{
 			TermQuery filter = new TermQuery();
-			filter.setWordFieldtypeDoctype("Jyoti", 1, 1);
-			//byte[] cellBytes = stu.toBytes(0);
-			//Cell2<Integer, Float> recordIds = stu.findIdsFromSerializedTableQuery(cellBytes, filter);
-			//System.out.println("Final Output :" + recordIds.getMap().size());
+			filter.setWordRecordtypeFieldType("searchField2", 1, 2);
+			byte[] cellBytes = tt.toBytes();
+			
+			Cell2<Integer, Float> recordIds = 
+					new Cell2<Integer, Float>(SortedBytesInteger.getInstance(), SortedBytesFloat.getInstance());
+			Cell4<Integer, Integer, Integer, Float> records = 
+					new Cell4<Integer, Integer, Integer, Float>(SortedBytesInteger.getInstance(), 
+							SortedBytesInteger.getInstance(), SortedBytesInteger.getInstance(), 
+							SortedBytesFloat.getInstance());
+			
+			tt.findIdsFromSerializedTableQuery(cellBytes, filter, recordIds, records);
+			
+			for (CellKeyValue<Integer, Float> ckv : recordIds.getMap())
+			{
+				System.out.println("Final Output :" + ckv.getKey() +", " +ckv.getValue());
+			}
 		} 
 		catch (Exception e) 
 		{
